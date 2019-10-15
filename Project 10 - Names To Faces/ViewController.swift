93ad2a0ccb3827f0d57e292798d358acc4f238d1
Sean-Williamsgit config --global user.email sean.r.williams85@gmail.comgit config --global user.name Sean-Williams <wilmslo@gmail.com>
2019-10-15 13:56:31 +0100

    Initial Commit

---
+++ Project 10 - Names To Faces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project 10 - Names To Faces
+//
+//  Created by Sean Williams on 15/10/2019.
+//  Copyright © 2019 Sean Williams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
